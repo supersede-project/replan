2153c9b16fb66d180de20805be85bf94cbe5d9a4
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2016-10-18 18:08:06 +0200

    Fix Gradle configuration and main class to run the service deployed as WAR in a J2EE container

--- replan_optimizer/src/main/java/io/swagger/Swagger2SpringBoot.java
+++ replan_optimizer/src/main/java/io/swagger/Swagger2SpringBoot.java
@@ -1,16 +1,29 @@
 package io.swagger;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.ExitCodeGenerator;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
 @ComponentScan(basePackages = "io.swagger")
+public class Swagger2SpringBoot extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Swagger2SpringBoot.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+	SpringApplication.run(Swagger2SpringBoot.class, args);
+    }
+}
+
+/*
 public class Swagger2SpringBoot implements CommandLineRunner {
 
 	@Override
@@ -34,3 +47,4 @@ public class Swagger2SpringBoot implements CommandLineRunner {
 
 	}
 }
+*/
