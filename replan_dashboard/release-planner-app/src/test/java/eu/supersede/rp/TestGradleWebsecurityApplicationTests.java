ab1d555307d41d8f897fccf6d6c383759ce63de8
antoninoCassarino <antonino.cassarino@gmail.com>
2017-03-23 19:24:39 +0100

    new test

--- replan_dashboard/release-planner-app/src/test/java/eu/supersede/rp/TestGradleWebsecurityApplicationTests.java
+++ replan_dashboard/release-planner-app/src/test/java/eu/supersede/rp/TestGradleWebsecurityApplicationTests.java
@@ -14,22 +14,38 @@
 
 package eu.supersede.rp;
 
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.test.context.web.WebAppConfiguration;
+//
+//import eu.supersede.rp.Application;
+//
+//import org.springframework.boot.test.SpringApplicationConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(classes = Application.class)
+//@WebAppConfiguration
+//public class TestGradleWebsecurityApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//}
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import eu.supersede.rp.Application;
-
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
-@WebAppConfiguration
-public class TestGradleWebsecurityApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class TestGradleWebsecurityApplicationTests
+{
+    @Test
+    public void contextLoads()
+    {
+    }
 }
