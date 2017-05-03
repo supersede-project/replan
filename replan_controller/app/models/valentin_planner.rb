3ea421753c7c6c78ea54df3afb0a60c70164e047
Carles Farre <carles.farre@gmail.com>
2017-05-03 15:19:26 +0000

    replan_optimizer endpoint set to local

--- replan_controller/app/models/valentin_planner.rb
+++ replan_controller/app/models/valentin_planner.rb
@@ -5,8 +5,8 @@ class ValentinPlanner
     # Your code here
   # uri = "http://replan-optimizer.herokuapp.com/replan"
   # uri = "http://platform.supersede.eu:8280/replan_optimizer/replan"
-    uri = "http://62.14.219.13:8280/replan_optimizer/replan"
-  # uri = "http://localhost:8280/replan_optimizer/replan"
+  # uri = "http://62.14.219.13:8280/replan_optimizer/replan"
+    uri = "http://localhost:8280/replan_optimizer/replan"
     payload = self.build_payload(release)
     puts "\nCalling replan_optimizer (#{uri}) with payload = #{payload}\n"
     response = RestClient.post uri, payload,  {content_type: :json, accept: :json}
