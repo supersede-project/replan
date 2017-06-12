1bcadbeffc58607d8dfddfa02619b20aa99d33a6
AndresInsaurralde <andres.insaurralde@est.fib.upc.edu>
2017-06-12 21:48:24 +0200

    Return PlanningSolution instead of PlanningSolutionWrapper

--- replan_optimizer/src/main/java/wrapper/SolverNRP.java
+++ replan_optimizer/src/main/java/wrapper/SolverNRP.java
@@ -123,7 +123,7 @@ public class SolverNRP {
 
         clearSolutionIfNotValid(solution);
 
-        return new PlanningSolutionWrapper(solution, solution.getPlannedFeatures());
+        return solution;
     }
 
     public PlanningSolution executeNRP(int nbWeeks, Number hoursPerweek, List<Feature> features,
@@ -140,7 +140,7 @@ public class SolverNRP {
 
         clearSolutionIfNotValid(solution);
 
-        return new PlanningSolutionWrapper(solution, solution.getPlannedFeatures());
+        return solution;
     }
 
     /*
