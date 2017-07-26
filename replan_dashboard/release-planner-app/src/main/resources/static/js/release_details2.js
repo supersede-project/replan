329806c5cafdd4a8ebd970d3afd7027111186694
antoninoCassarino <antonino.cassarino@gmail.com>
2017-07-26 14:09:39 +0200

    improvement release details v2 screen

--- replan_dashboard/release-planner-app/src/main/resources/static/js/release_details2.js
+++ replan_dashboard/release-planner-app/src/main/resources/static/js/release_details2.js
@@ -93,11 +93,7 @@ app.controllerProvider.register('release-details2', ['$scope', '$location', '$ht
 		$scope.getReleaseFeatures(releaseId)
 		.then(
 				function(response) {
-
-					//var myFeatures = '[{"id":118,"code":5065,"name":"ahp requirement 1","description":"","effort":"5.0","deadline":"2017-05-01","priority":5,"required_skills":[{"id":21,"name":"Java","description":"Object oriented programming"}],"depends_on":[],"release":{"release_id":16}},{"id":119,"code":5066,"name":"ahp requirement 2","description":"","effort":"10.0","deadline":"2017-05-17","priority":2,"required_skills":[{"id":21,"name":"Java","description":"Object oriented programming"}],"depends_on":[],"release":{"release_id":16}},{"id":120,"code":5067,"name":"ahp requirement 3","description":"","effort":"9.0","deadline":"2017-05-18","priority":2,"required_skills":[{"id":21,"name":"Java","description":"Object oriented programming"}],"depends_on":[{"id":119,"code":5066,"name":"ahp requirement 2","description":"","effort":"10.0","deadline":"2017-05-17","priority":2,"release":{"release_id":16}}],"release":{"release_id":16}},{"id":121,"code":5068,"name":"ahp requirement 4","description":"","effort":"1.0","deadline":"2017-05-25","priority":5,"required_skills":[{"id":21,"name":"Java","description":"Object oriented programming"}],"depends_on":[{"id":118,"code":5065,"name":"ahp requirement 1","description":"","effort":"5.0","deadline":"2017-05-01","priority":5,"release":{"release_id":16}},{"id":119,"code":5066,"name":"ahp requirement 2","description":"","effort":"10.0","deadline":"2017-05-17","priority":2,"release":{"release_id":16}}],"release":{"release_id":16}},{"id":200,"code":5068,"name":"barbara","description":"","effort":"1.0","deadline":"2017-05-25","priority":5,"required_skills":[{"id":21,"name":"Java","description":"Object oriented programming"}],"depends_on":[{"id":118,"code":5065,"name":"ahp requirement 1","description":"","effort":"5.0","deadline":"2017-05-01","priority":5,"release":{"release_id":16}},{"id":119,"code":5066,"name":"ahp requirement 2","description":"","effort":"10.0","deadline":"2017-05-17","priority":2,"release":{"release_id":16}}],"release":{"release_id":16}}]';
-					//var obj = JSON.parse(myFeatures);
-					//$scope.releaseFeatures = obj;
-
+					
 					$scope.releaseFeatures = response.data;
 
 					$scope.getRelease(releaseId)
@@ -117,15 +113,12 @@ app.controllerProvider.register('release-details2', ['$scope', '$location', '$ht
 											$scope.planJqxgrid = JSON.parse(JSON.stringify(responseData)); 
 											addPropertiesTOPlanJqxgrid();
 											$scope.initFeaturesJqxgrid();
-										
-											//google chart
-											
-											google.charts.setOnLoadCallback(drawTimelineChart);
-											google.charts.setOnLoadCallback(drawGanttChart);
-											
-											//drawTimelineChart();
-											//drawGanttChart();
 
+											//google charts
+											
+											//drawGantChart and then drawTimelineChart
+											google.charts.setOnLoadCallback(drawGanttChart);
+										
 										},
 										function(response) {
 											$scope.showReleasePlan = false;
@@ -310,25 +303,26 @@ app.controllerProvider.register('release-details2', ['$scope', '$location', '$ht
 				        		  //remove from scope
 				        		  $scope.planJqxgrid.jobs.splice(row, 1);
 				        		  $scope.plan.jobs.splice(row, 1);
-				        		  
-				        		  for(var i=0; i< $scope.plan.jobs.legth; i++){
-				        			
-				        			for (var j = $scope.plan.jobs[i].depends_on.length -1; j >= 0 ; j--) {
-										if($scope.plan.jobs[i].depends_on[j].id == $scope.planJqxgrid.jobs[row].feature.id){
-											elements.splice(j, 1);
-										}
-									}  
-				        			
-				        			
+
+				        		  for(var i=0; i< $scope.plan.jobs.length; i++){
+
+				        			  for (var j = $scope.plan.jobs[i].depends_on.length -1; j >= 0 ; j--) {
+				        				  if($scope.plan.jobs[i].depends_on[j].id == $scope.planJqxgrid.jobs[row].feature.id){
+				        					  elements.splice(j, 1);
+				        				  }
+				        			  }  
+
+
 				        		  }
-				        		  
-				        		  
+
+
 				        		  //redraw the chart
 				        		  //$scope.draw();
-				        		  //google chart
-								  drawTimelineChart();
-								  drawGanttChart();
-									
+
+				        		  //google charts
+				        		  
+				        		  //drawGantChart and then drawTimelineChart
+				        		  drawGanttChart();
 				        		  //refresh the table
 				        		  $("#featuresJqxgrid").jqxGrid("updatebounddata", "cells");
 				        		  //}
@@ -358,6 +352,127 @@ app.controllerProvider.register('release-details2', ['$scope', '$location', '$ht
 		$scope.blncreateFeaturesJqxgrid = true;
 
 	};
+
+	var mappingIDColorJSONObject = {};
+	mappingIDColorJSONObject["array"] = [];
+
+	
+	function ganttChartReadyHandler() {
+		$scope.parse();
+	}
+	
+	$scope.parse = function(){
+	
+		var gantt_chart = $('#gantt_chart');
+		var child0 = gantt_chart[0];
+		var child1 = child0.children[0];
+		var child2 = child1.children[0];
+		var svg = child2.children[0];
+		for(var i=0; i< svg.children.length; i++){
+			//defs,g
+			var node = svg.children[i];
+
+			for(var j=0; j< node.children.length; j++){
+				var node1 = node.children[j];
+				if(node1.tagName == "text"){
+
+					if(isFeature(node1.textContent)){
+						var stringArray = node1.outerHTML.split(" ");
+						for(var z=0; z< stringArray.length; z++){
+							if(stringArray[z].indexOf("fill") !== -1){
+								var fillArray = stringArray[z].split("=");
+								var color = fillArray[1];
+								var JSONObject = {};
+								JSONObject.id=node1.textContent;
+								
+								JSONObject.color=color.replace(/["']/g, "");
+								mappingIDColorJSONObject.array.push(JSONObject);
+							}
+						}
+					}
+				}
+			} 
+		}
+
+		//http://jsfiddle.net/sirko/0q52s2c4/1/
+//		$('#gantt_chart').children('svg').children('text').each(function () {
+//		alert( $(this).attr('fill'));
+//		});
+
+		console.log("element");  
+		google.charts.setOnLoadCallback(drawTimelineChart);
+	}
+
+	function isFeature(textContent){
+
+		for(var i=0; i< $scope.plan.jobs.length; i++){
+			if(""+$scope.plan.jobs[i].feature.id ==  textContent){
+				return true;
+			}
+
+		}
+		return false;
+	}
+//	function recurseDomChildren(start, output)
+//	{
+//	var nodes;
+//	if(start.childNodes)
+//	{
+//	nodes = start.childNodes;
+//	loopNodeChildren(nodes, output);
+//	}
+//	}
+
+//	function loopNodeChildren(nodes, output)
+//	{
+//	var node;
+//	for(var i=0;i<nodes.length;i++)
+//	{
+//	node = nodes[i];
+//	if(output)
+//	{
+//	outputNode(node);
+//	}
+//	if(node.childNodes)
+//	{
+//	recurseDomChildren(node, output);
+//	}
+//	}
+//	}
+
+//	function outputNode(node)
+//	{
+
+//	//if(node.nodeName == "svg" ){
+//	//console.log("cu cu");
+
+//	var whitespace = /^\s+$/g;
+//	//element node
+//	if(node.nodeType === 1)
+//	{
+//	console.log("element: " + node.tagName);  
+//	}
+//	//text node
+//	else if(node.nodeType === 3)
+//	{
+//	//clear whitespace text nodes
+//	node.data = node.data.replace(whitespace, "");
+//	if(node.data)
+//	{
+//	console.log("text: " + node.data); 
+
+//	var fill = node.attr('fill');
+//	alert(fill);
+//	//var uniq = '' + (new Date()).getTime();
+//	//node.id="pippo";
+
+//	//var element = document.getElementById(uniq);
+//	//console.log("text: " + element); 
+//	}  
+//	} 
+
+//	//}
+//	}
 
 	function is_in_featuresTORemove(id) {
 		for(var i = 0; i<$scope.featuresTORemove.length; ++i){ 
@@ -462,52 +577,14 @@ app.controllerProvider.register('release-details2', ['$scope', '$location', '$ht
 		return arr; 
 	}
 
+
 	/**
 	 * Google chart functions
 	 */
-	
-	function drawTimelineChart() {
-		
-		var container = document.getElementById('timeline_chart');
-		var timelineChart = new google.visualization.Timeline(container);
-		var dataTable = new google.visualization.DataTable();
 
-		dataTable.addColumn({ type: 'string', id: 'Developer' });
-		dataTable.addColumn({ type: 'string', id: 'TaskName' });
-		dataTable.addColumn({ type: 'date', id: 'Start' });
-		dataTable.addColumn({ type: 'date', id: 'End' });
-
-		var numberOfRows =  dataTable.getNumberOfRows();
-		if(numberOfRows > 0){
-			dataTable.removeRows(0, numberOfRows)
-		}
-		
-		var jobs = $scope.plan.jobs;
-		for(var i = jobs.length-1; i>=0; i--){
-			var job = jobs[i];
-			var array = [];
-			array[0] = job.resource.name;
-			array[1] = ""+job.feature.id+"(Id)-"+job.feature.name;
-			array[2] = getDate(job.starts);
-			array[3] = getDate(job.ends);
-
-			dataTable.addRow(array);
-		}
-		
-		var trackHeight = 20;
-		
-		var options = {
-				height: dataTable.getNumberOfRows() * trackHeight,
-				timeline: {
-					colorByRowLabel: true
-				}
-		};
-
-		timelineChart.draw(dataTable, options);
-	}
-	
 	function drawGanttChart() {
-
+		
+		//1.create dataTable
 		var dataTable = new google.visualization.DataTable();
 		dataTable.addColumn('string', 'Task ID');
 		dataTable.addColumn('string', 'Task Name');
@@ -517,13 +594,15 @@ app.controllerProvider.register('release-details2', ['$scope', '$location', '$ht
 		dataTable.addColumn('number', 'Duration');
 		dataTable.addColumn('number', 'Percent Complete');
 		dataTable.addColumn('string', 'Dependencies');
+		
+		//2.add rows in dataTable
 		var arrays = [];
 		var jobs = $scope.plan.jobs;
 		for(var i = jobs.length-1; i>=0; i--){
 			var job = jobs[i];
 			var array = [];
 			array[0] = ""+job.id;
-			array[1] = ""+job.feature.id+"(Id)-"+job.feature.name;
+			array[1] = ""+job.feature.id;
 			array[2] = job.resource.name;
 			array[3] = getDate(job.starts);
 			array[4] = getDate(job.ends);
@@ -538,24 +617,154 @@ app.controllerProvider.register('release-details2', ['$scope', '$location', '$ht
 				}
 			}
 			array[7] = depedencies;
-			
+
 			arrays[i] = array;
-			
 		}
+
 		dataTable.addRows(arrays);
-		var ganttChart = new google.visualization.Gantt(document.getElementById('gantt_chart'));
 		
+		//3 initialize
+		var ganttChart = new google.visualization.Gantt(document.getElementById('gantt_chart'));
+
 		var trackHeight = 50;
 		var options = {
-				height: dataTable.getNumberOfRows() * trackHeight
-			
+				height: dataTable.getNumberOfRows() * trackHeight		
 		};
+		//3.1 draw
 		ganttChart.draw(dataTable, options);
+		
+		//3.2 add listener
+		google.visualization.events.addListener(ganttChart, 'ready', ganttChartReadyHandler);
+
 	}	
 
-	
+	function drawTimelineChart() {
 
+		//1.create dataTable
+		var dataTable = new google.visualization.DataTable();
+
+		dataTable.addColumn({ type: 'string', id: 'Developer' });
+		dataTable.addColumn({ type: 'string', id: 'TaskName' });
+		dataTable.addColumn({'type': 'string', 'role': 'tooltip', 'p': {'html': true}});
+		dataTable.addColumn({ type: 'date', id: 'Start' });
+		dataTable.addColumn({ type: 'date', id: 'End' });
+
+		var numberOfRows =  dataTable.getNumberOfRows();
+		if(numberOfRows > 0){
+			dataTable.removeRows(0, numberOfRows)
+		}
+		
+		//2.add rows in dataTable
+		var arrays = [];
+		var jobs = $scope.plan.jobs;
+		for(var i = jobs.length-1; i>=0; i--){
+			var job = jobs[i];
+			var array = [];
+			
+
+			array[0] = job.resource.name;
+			array[1] = ""+job.feature.id;
+
+			var startDate = getDate(job.starts);
+			var endDate = getDate(job.ends);
+
+			var hours = Math.abs(startDate - endDate) / 3600000;
+			var hourLabel = "hour"; 
+			if(hours>10){
+				hourLabel = "hours";
+			}
+			var depends_on = "";
+			for(var z=0; z<job.depends_on.length; z++){
+				if(z==0){
+					depends_on = job.depends_on[z].id;	
+				}else{
+					depends_on = depends_on + ", " + job.depends_on[z].id;
+				}
+
+			}
+			//tooltip
+			array[2] = "<div>" +
+			"<table class='tooltip_table'>" +
+			"<tr>" +
+			"<th>" +
+			"<b>" + job.feature.name + "<b>" +
+			"</th>" +
+			"</tr>" +
+			"<tr>" +
+			"<td>" +
+			"<b>Id:</b> "+ job.feature.id + "<br>" +
+			"<b>Start:</b> "+ getDateASString(job.starts) + "<br>" +
+			"<b>End:</b> "+getDateASString(job.ends) + "<br>" +
+			"<b>Duration:</b> " + hours+ " " + hourLabel + "<br>" +
+			"<b>Effort:</b> " + job.feature.effort + "<br>" +
+			"<b>Priority:</b> "+ job.feature.priority + "<br>" +
+			"<b>Depends on:</b> "+ depends_on + "<br>" +
+			"</td>" +
+			"</tr>" +
+			"</table>" +
+			"</div>"
+			array[3] = getDate(job.starts);
+			array[4] = getDate(job.ends);
+
+			
+			arrays.push(array);
+
+		}
+		dataTable.addRows(arrays);
+		
+		//3 initialize
+		var container = document.getElementById('timeline_chart');
+		var timelineChart = new google.visualization.Timeline(container);
 	
+	
+		//calculate option color array
+		var trackHeight = 25;
+		var colorsArray = [];
+		for(var j=0; j<arrays.length; j++){
+			var element = arrays[j];
+			for(var z=0; z<mappingIDColorJSONObject.array.length; z++){
+				if(element[1] == mappingIDColorJSONObject.array[z].id){
+					colorsArray.push(mappingIDColorJSONObject.array[z].color);
+				}
+			}
+		}		
+		colorsArray = array_unique(colorsArray);
+
+		var options = {
+				height: dataTable.getNumberOfRows() * trackHeight,
+				timeline: {
+					colorByRowLabel: true
+				}
+		,
+		tooltip: { isHtml: true },
+		//colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6']
+		colors: colorsArray
+		};
+
+		//3.1 draw
+		timelineChart.draw(dataTable, options);
+
+	}
+	
+	function getDateASString(dateAsString) {
+		var res = dateAsString.split("T");
+		var time = res[1].split(":");
+		var my_date= time[0] +":" + time[1];
+		return my_date;
+	}
+
+	function array_unique(arr) {
+		var result = [];
+		for (var i = 0; i < arr.length; i++) {
+			if (result.indexOf(arr[i]) == -1) {
+				result.push(arr[i]);
+			}
+		}
+		return result;
+	}
+
+
+
 	/**
 	 * start point function
 	 */
