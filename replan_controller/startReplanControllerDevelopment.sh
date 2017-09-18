9c7ace5d96994667928078689f3d5e4ec2b15dcd
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2017-09-18 18:33:54 +0200

    Fix bundle update when building replan

--- replan_controller/startReplanControllerDevelopment.sh
+++ replan_controller/startReplanControllerDevelopment.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-/usr/local/rvm/gems/ruby-2.4.1/bin/bundle update
-/usr/local/rvm/gems/ruby-2.4.1/bin/rake db:migrate
+rvm --default use ruby-2.4.1
+bundle update
+rake db:migrate
 #rake db:seed
 BUILD_ID=dontKillMe nohup rails server -d -b 192.168.145.13 -p 3000 &
