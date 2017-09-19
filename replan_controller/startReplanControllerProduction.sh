68ce62e59c6584d5939399fea1d09586c92edbd2
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2017-09-19 14:09:14 +0200

    Added logging information to launcher

--- replan_controller/startReplanControllerProduction.sh
+++ replan_controller/startReplanControllerProduction.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+echo "Installing Replan Controler"
 bundle install
+echo "Migrating the DB"
 rake db:migrate
 #rake db:seed
+echo "Launching Replan Controller"
 BUILD_ID=dontKillMe nohup rails server -d -b 217.194.3.60 -p 3000 &
