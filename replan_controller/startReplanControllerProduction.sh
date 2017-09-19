d391ffe7f5b70d087ec3c166457b55b2303f6253
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2017-09-19 14:14:00 +0200

    Fix launching replan controller

--- replan_controller/startReplanControllerProduction.sh
+++ replan_controller/startReplanControllerProduction.sh
@@ -5,4 +5,5 @@ echo "Migrating the DB"
 rake db:migrate
 #rake db:seed
 echo "Launching Replan Controller"
-BUILD_ID=dontKillMe nohup rails server -d -b 217.194.3.60 -p 3000 &
+#BUILD_ID=dontKillMe nohup rails server -d -b 217.194.3.60 -p 3000 &
+daemonize -E BUILD_ID=dontKillMe nohup rails server -d -b 217.194.3.60 -p 3000 &
